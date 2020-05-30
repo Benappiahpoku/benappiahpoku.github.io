628efd7157e431d1d3159d139dabf7befd1ac84a
benjamin <bappiahpoku@gmail.com>
2020-05-30 11:00:18 +0000

    update commit

--- jda/dist/js/main.js
+++ jda/dist/js/main.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
    $(".header").height($(window).height());
 });
 
-$(".navbar a").click(function() {
+$(".nav-link").click(function(evt) {
+    evt.preventDefault();
     $("body,html").animate(
         {
             scrollTop: $("#" + $(this).data("value")).offset().top
